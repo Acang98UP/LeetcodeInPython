8f39201887b49ca0a2ee4aa1aec3b066358b3975
Acang98UP <1272371331@qq.com>
2020-12-13 15:50:27 +0800

    12 Dec 摆动序列

--- 376-摆动序列-Mid/wiggleSubsequence.py
+++ 376-摆动序列-Mid/wiggleSubsequence.py
@@ -2,8 +2,19 @@ from typing import List
 
 
 def wiggleMaxLength(nums: List[int]) -> int:
-
-    return 0
+    # c是用来记录前一个差值是下降还是上升的，默认0
+    n, c, res = len(nums), 0, 1
+    if n < 2:
+        return n
+    for i in range(1, n):
+        x = nums[i] - nums[i - 1]
+        # 如果有差值才继续处理，相等直接就跳过不处理了
+        if x:
+            # <0代表有上升下降的交替，=0是初始情况的判断
+            if x * c <= 0:
+                res += 1
+            c = x
+    return res
 
 def main():
     testSequence = [1, 7, 4, 9, 2, 5]
