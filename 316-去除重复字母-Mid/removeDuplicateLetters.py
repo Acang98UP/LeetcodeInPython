0c41a9886a70d718bef1da5d2c0a05234e958752
Acang98UP <1272371331@qq.com>
2020-12-20 15:17:17 +0800

    20 Dec 去除重复字母

--- 316-去除重复字母-Mid/removeDuplicateLetters.py
+++ 316-去除重复字母-Mid/removeDuplicateLetters.py
@@ -9,6 +9,9 @@ def removeDuplicateLetters(s: str) -> str:
     stack = []
     for i in s:
         if i not in stack:
+            # 此处的判断条件是关键：首先栈中保存的是最后的结果
+            # i < stack[-1]: 这个条件就是判断字典序的大小，为了避免误删，所以有了后面的条件
+            # str_dict[stack[-1]] > 0:这个条件就是在保证后面还有的情况下，把小的安排在前面
             while stack and i < stack[-1] and str_dict[stack[-1]] > 0:
                 stack.pop()
             stack.append(i)
