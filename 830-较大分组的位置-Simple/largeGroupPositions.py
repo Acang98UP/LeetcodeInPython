d7356b7fd28e4a90a33d8a1a004566029d9718a3
Acang98UP <1272371331@qq.com>
2021-01-05 14:56:43 +0800

    5 Jan 较大分组的位置,要学会在字符串左右加一个空值！！

--- 830-较大分组的位置-Simple/largeGroupPositions.py
+++ 830-较大分组的位置-Simple/largeGroupPositions.py
@@ -6,15 +6,13 @@ def largeGroupPositions(s: str) -> List[List[int]]:
         return []
     res = []
     stack = []
+    s = s + ' '
     begin, end = 0, 0
     for i in range(len(s)):
         if s[begin] == s[i]:
             end = i
             if (end - begin + 1) >= 3:
                 stack.append(end)
-                if i == len(s) - 1:
-                    res.append([begin, stack[-1]])
-                    stack.clear()
         else:
             while stack:
                 res.append([begin, stack[-1]])
