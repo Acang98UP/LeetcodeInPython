76d2f1733ea047e807443826a782f8bfb6ef467f
User <zhang204ever@qq.com>
2021-02-01 16:57:38 +0800

    1 Feb 简单题目

--- 888-公平的糖果棒交换-Simple/fairCandySwap.py
+++ 888-公平的糖果棒交换-Simple/fairCandySwap.py
@@ -1,12 +1,20 @@
+import collections
 from typing import List
 
 
 def fairCandySwap(A: List[int], B: List[int]) -> List[int]:
-    pass
+    diff = sum(B) - sum(A)
+    dic = collections.Counter(A)
+    print(dic)
+    for b in B:
+        if b - diff // 2 in dic:
+            return [b - diff // 2, b]
 
 
 def main():
-    pass
+    A = [1, 2, 5]
+    B = [2, 4]
+    print(fairCandySwap(A, B))
 
 
 if __name__ == '__main__':
