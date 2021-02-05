79e0fbcea57d30b182f9a4a151e722b46ff19414
Acang98UP <1272371331@qq.com>
2021-02-05 13:56:53 +0800

    5 Feb 滑动窗口题目模板题

---
+++ 1208-尽可能使字符串相等-Mid/equalSubstring.py
@@ -0,0 +1,13 @@
+def equalSubstring(s: str, t: str, maxCost: int) -> int:
+    pass
+
+
+def main():
+    s = 'abcd'
+    t = 'bcdf'
+    cost = 3
+    print(equalSubstring(s, t, cost))
+
+
+if __name__ == '__main__':
+    main()
