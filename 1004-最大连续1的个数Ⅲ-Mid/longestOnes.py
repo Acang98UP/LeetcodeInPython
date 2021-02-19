3da630e502707a8372b88ad885aebf4b1fe1cdad
Acang98UP <1272371331@qq.com>
2021-02-19 10:41:33 +0800

    18 Feb 滑动窗口套路

---
+++ 1004-最大连续1的个数Ⅲ-Mid/longestOnes.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+def longestOnes(A: List[int], K: int) -> int:
+    N = len(A)
+    res = 0
+    left, right = 0, 0
+    zeros = 0
+    while right < N:
+        if A[right] == 0:
+            zeros += 1
+        while zeros > K:
+            if A[left] == 0:
+                zeros -= 1
+            left += 1
+        res = max(res, right - left + 1)
+        right += 1
+    return res
+
+
+def main():
+    A = [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0]
+    K = 2
+    print(longestOnes(A, K))
+
+
+if __name__ == '__main__':
+    main()
