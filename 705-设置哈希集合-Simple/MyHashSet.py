bc926ace1d16347e5a610de1d85d34d1250b017a
Acang98UP <1272371331@qq.com>
2021-03-13 22:25:03 +0800

    13 Mar 哈希表的Python实现-拉链法

---
+++ 705-设置哈希集合-Simple/MyHashSet.py
@@ -0,0 +1,33 @@
+class MyHashSet:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.buckets = 1000
+        self.itemsPerBucket = 1001
+        self.table = [[] for _ in range(self.buckets)]
+
+    def hash(self, key):
+        return key % self.buckets
+
+    def pos(self, key):
+        return key // self.buckets
+
+    def add(self, key: int) -> None:
+        hashkey = self.hash(key)
+        if not self.table[hashkey]:
+            self.table[hashkey] = [0] * self.itemsPerBucket
+        self.table[hashkey][self.pos(key)] = 1
+
+    def remove(self, key: int) -> None:
+        hashkey = self.hash(key)
+        if self.table[hashkey]:
+            self.table[hashkey][self.pos(key)] = 0
+
+    def contains(self, key: int) -> bool:
+        """
+        Returns true if this set contains the specified element
+        """
+        hashkey = self.hash(key)
+        return (self.table[hashkey] != []) and (self.table[hashkey][self.pos(key)] == 1)
