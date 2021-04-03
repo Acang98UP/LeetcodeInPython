83b98666b5d4abb1a98c9a03ec2f81dac02d562a
Acang98UP <1272371331@qq.com>
2021-04-03 16:42:49 +0800

    3 Apr 二维动态规划题

---
+++ 1143-最长公共子序列-Mid/longestCommonSubsequence.py
@@ -0,0 +1,20 @@
+def longestCommonSubsequence(text1: str, text2: str) -> int:
+    M, N = len(text1), len(text2)
+    dp = [[0] * (N + 1) for _ in range(M + 1)]
+    for i in range(1, M + 1):
+        for j in range(1, N + 1):
+            if text1[i - 1] == text2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+    return dp[M][N]
+
+
+def main():
+    text1 = 'abcde'
+    text2 = 'ace'
+    print(longestCommonSubsequence(text1, text2))
+
+
+if __name__ == '__main__':
+    main()
