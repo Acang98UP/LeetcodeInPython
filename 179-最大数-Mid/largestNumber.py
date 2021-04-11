a3aa12a35b0ccd49c9a1ec48ee37e78672d4c689
Acang98UP <1272371331@qq.com>
2021-04-12 00:57:48 +0800

    12 Apr 最大数，涉及数字与字符串的转换的使用

---
+++ 179-最大数-Mid/largestNumber.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class StrLt(str):
+    def __lt__(x, y):
+        return x+y > y+x
+
+
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        strn = [str(n) for n in nums]
+        strn.sort(key=StrLt)
+        return ''.join(strn) if strn[0] != '0' else '0'
+
+
